c64bab87cfb2542f6e19eb8f2130307a519a3b33
Andre Emidio <andre@datatype.com.br>
2020-07-28 21:36:02 -0300

    Criação do servidor de OCR

---
+++ server.py
@@ -0,0 +1,30 @@
+#####pythoncode.py#####
+import numpy as np
+import sys, os
+from fastapi import FastAPI, UploadFile, File
+from starlette.requests import Request
+import io
+import cv2
+import pytesseract
+from pydantic import BaseModel
+
+
+def read_img(img):
+     text = pytesseract.image_to_string(img)
+     return(text)
+
+app = FastAPI()
+class ImageType(BaseModel):
+    url: str
+    
+    
+@app.post ("/predict/") 
+def prediction(request: Request,file: bytes = File(...)):
+    if request.method == "POST":
+        image_stream = io.BytesIO(file)
+        image_stream.seek(0)
+        file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
+        frame = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+        label = read_img(frame)
+        return label
+    return "No post request found"
